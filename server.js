4aea1459900cac1e13ac43d8f8164fccdbe9afcf
imagentleman <imagentlemail@gmail.com>
2014-03-28 00:25:19 -0500

    Create server.js

---
+++ server.js
@@ -0,0 +1,24 @@
+var app = require('http').createServer(handler)
+  , io = require('socket.io').listen(app)
+  , fs = require('fs')
+
+app.listen(80);
+
+function handler (req, res) {
+  fs.readFile(__dirname + '/index.html',
+  function (err, data) {
+    if (err) {
+      res.writeHead(500);
+      return res.end('Error loading html');
+    }
+
+    res.writeHead(200);
+    res.end(data);
+  });
+}
+
+io.sockets.on('connection', function (socket) {
+  socket.on('relay', function (data) {
+    socket.broadcast.emit('relaying', {x: data.x, y: data.y});
+  });
+});
